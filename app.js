1819ddf469a0ad902ab57a78a8fa373e4394b85f
payalpagaria <pagariapayal67@gmail.com>
2023-02-01 23:56:51 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ import bodyParser from 'body-parser';
 const PORT=3000;
 const app=express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/",(req,res)=>{
+    res.send("Hey developer")
+})
 app.listen(PORT,console.log(`The server has started on port ${PORT}`));

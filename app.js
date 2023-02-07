4412a776677c9b1ee4d35385be2159f0c919f3f3
payalpagaria <pagariapayal67@gmail.com>
2023-02-07 22:34:07 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import ejs from 'ejs';
 const PORT=3000;
 const app=express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
     res.send("Hey developer")
 })
